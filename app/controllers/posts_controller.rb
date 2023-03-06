5d330195287e5e2451b10152886601913833b4f7
mikemoriarty213 <mike.moriarty@kellogg.northwestern.edu>
2023-03-06 19:19:21 +0000

    updated posts controller to include login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+ 
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
@@ -6,13 +10,19 @@ class PostsController < ApplicationController
   end
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
+    @post["image"] = params["post"]["image"]
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = @current_user["id"]
     @post.save
+  else
+    flash["notice"] = "Login first."
+  end
     redirect_to "/places/#{@post["place_id"]}"
   end
-
 end
+
